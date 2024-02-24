89c1be1999bd62e785561d2f02a9ccba4e208ebf
Gemagit <gemitaphotographer@gmail.com>
2024-02-24 22:57:55 +0100

    version1/modified assets/modified script.js/style.css/indez.html

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let boton = document.getElementsByClassName("botoncalavera");
+
+boton.addEventListener("click", function() {
+    window.location.href = "./quiz.html";
+});
+
+
+
+const respuestas = {
+    "defLepard":"Def Lepard",
+    
+}
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+  
+    const respuesta1 = event.target["defLepard"]["Def Lepard"];
+    const respuesta2= event.target.name.value;
+    
+  
+    let msj = "";
+  
+    if (respuesta1=="Def Lepard") {
+      return respuesta1.style.background="green";
+    
+    if (respuesta !=="Def Lepard") {
+        return respuesta.style.background="red";
+    }
+
+    }
+    else {
+      alert("Respuestas enviadas correctamente");
+      event.target.submit();
+    }
+  });
+  
+  
