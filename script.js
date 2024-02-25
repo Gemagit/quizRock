cf553510014956e43f887b0380fb800114394577
Gemagit <gemitaphotographer@gmail.com>
2024-02-25 15:03:36 +0100

    modified index.html/quiz.html/scrypt.js/v2

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 
-let boton = document.getElementsByClassName("botoncalavera");
+let boton = document.getElementById("myButton");
 
-boton.addEventListener("click", function() {
-    window.location.href = "./quiz.html";
+boton.addEventListener("click", function () {
+    window.location.href ="./quiz.html";
 });
 
 
 
 const respuestas = {
-    "defLepard":"Def Lepard",
-    
+    "defLepard": "Def Lepard",
+
 }
 
 document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault(); 
-  
+    event.preventDefault();
+
     const respuesta1 = event.target["defLepard"]["Def Lepard"];
-    const respuesta2= event.target.name.value;
-    
-  
+    const respuesta2 = event.target.name.value;
+
+
     let msj = "";
-  
-    if (respuesta1=="Def Lepard") {
-      return respuesta1.style.background="green";
-    
-    if (respuesta !=="Def Lepard") {
-        return respuesta.style.background="red";
-    }
+
+    if (respuesta1 == "Def Lepard") {
+        return respuesta1.style.background = "green";
+
+        if (respuesta !== "Def Lepard") {
+            return respuesta.style.background = "red";
+        }
 
     }
     else {
-      alert("Respuestas enviadas correctamente");
-      event.target.submit();
+        alert("Respuestas enviadas correctamente");
+        event.target.submit();
     }
-  });
-  
-  
+});
+
