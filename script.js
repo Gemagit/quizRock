fd45ff6b179a8ddf3c39abfbf1f6998a65908e9b
Gemagit <gemitaphotographer@gmail.com>
2024-02-25 20:05:58 +0100

    modified index/script and style.es/v2

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 
-let boton = document.getElementById("myButton");
-
-boton.addEventListener("click", function () {
-    window.location.href ="./quiz.html";
-});
-
-
 
 const respuestas = {
-    "defLepard": "Def Lepard",
-
+    deflepard: "Def Lepard",
+    thunderbird: "Thunderbird",
+    america: "America",
+    agila: "Agila",
+    1997: "1997"
 }
 
 document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const respuesta1 = event.target["defLepard"]["Def Lepard"];
-    const respuesta2 = event.target.name.value;
+    console.dir(event.target);
 
+    for (let i = 0; i < respuestas.length; i++) {
+        respuestas[i].addEventListener("click", function () {
+            if(respuestas[i]==true) {
+                respuestas[i].style.backgroundColor = "green";
+            } else {
+                respuestas[i].style.backgroundColor = "red";
+            }
+        })
+    };
 
     let msj = "";
 
-    if (respuesta1 == "Def Lepard") {
-        return respuesta1.style.background = "green";
-
-        if (respuesta !== "Def Lepard") {
-            return respuesta.style.background = "red";
-        }
-
+    if (respuestas[i]<=3) {
+        alert ("¡Tienes que leer más la documentación!");
+    } if (respuestas[i]>=4) {
+        alert ("¡Enhorabuena, estás hecho un autético rockero");
     }
-    else {
-        alert("Respuestas enviadas correctamente");
+
+
+    if (msj.length != 0) {
+        alert(msj); //imprime mensaje final de error
+        //document.body.innerHTML += msj;
+        let p = document.createElement("pre");
+        let mensaje = document.createTextNode(msj);
+        p.style.color = "#DD1C1A";
+        p.style.fontSize = "16px";
+        p.appendChild(mensaje);
+    
+        document.getElementById("contact").appendChild(p); // dibuja resultado
+      }
+      else{
+        //Enviar formulario
+        //document.querySelector("form").submit();
+        alert("Formulario enviado con éxito");
         event.target.submit();
-    }
-});
+      }
+    });
 
