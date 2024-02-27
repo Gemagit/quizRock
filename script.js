7fb516d8b377b7a5bd8d238e6a34bd52f64c85e1
Gemagit <gemitaphotographer@gmail.com>
2024-02-27 22:01:46 +0100

    modified index/quiz.html/js is running better

--- script.js
+++ script.js
@@ -1,53 +1,59 @@
 
 
-const respuestas = {
-    deflepard: "Def Lepard",
-    thunderbird: "Thunderbird",
-    america: "America",
-    agila: "Agila",
-    1997: "1997"
+const respuestasCorrectas = {
+    pregunta1: "Def Lepard",
+    pregunta2: "Thunderbird",
+    pregunta3: "America",
+    pregunta4: "Agila",
+    pregunta5: "1997"
 }
+
+const respuestas = document.querySelectorAll(".respuesta");
+// Iterar sobre cada elemento y agregar un event listener
+respuestas.forEach(function(respuesta) {
+    respuesta.addEventListener("click", function() {
+        // Cambia el color de fondo del contenedor(respuesta)
+       respuesta.style.backgroundColor = "rgb(39, 48, 129)";
+    });
+});
+
+
 
 document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    console.dir(event.target);
+    const respuesta1 = event.target.pregunta1.value;
+    const respuesta2 = event.target.pregunta2.value;
+    const respuesta3 = event.target.pregunta3.value;
+    const respuesta4 = event.target.pregunta4.value;
+    const respuesta5 = event.target.pregunta5.value;
 
-    for (let i = 0; i < respuestas.length; i++) {
-        respuestas[i].addEventListener("click", function () {
-            if(respuestas[i]==true) {
-                respuestas[i].style.backgroundColor = "green";
-            } else {
-                respuestas[i].style.backgroundColor = "red";
-            }
-        })
-    };
-
-    let msj = "";
-
-    if (respuestas[i]<=3) {
-        alert ("¡Tienes que leer más la documentación!");
-    } if (respuestas[i]>=4) {
-        alert ("¡Enhorabuena, estás hecho un autético rockero");
-    }
-
-
-    if (msj.length != 0) {
-        alert(msj); //imprime mensaje final de error
-        //document.body.innerHTML += msj;
-        let p = document.createElement("pre");
-        let mensaje = document.createTextNode(msj);
-        p.style.color = "#DD1C1A";
-        p.style.fontSize = "16px";
-        p.appendChild(mensaje);
-    
-        document.getElementById("contact").appendChild(p); // dibuja resultado
-      }
-      else{
-        //Enviar formulario
-        //document.querySelector("form").submit();
-        alert("Formulario enviado con éxito");
-        event.target.submit();
-      }
+    const respuestas = document.querySelectorAll(".respuesta");
+// Iterar sobre cada elemento y agregar un event listener
+respuestas.forEach(function(respuesta) {
+    respuesta.addEventListener("click", function() {
+        if (respuesta[i] === respuestasCorrectas[i]){
+            respuesta.style.backgroundColor = "green";
+        } else {
+            respuesta.style.backgroundColor = "red";
+        }
     });
+});
+
+    let aciertos = 0;
+    if (respuesta1 === respuestasCorrectas.pregunta1) aciertos++;
+    if (respuesta2 === respuestasCorrectas.pregunta2) aciertos++;
+    if (respuesta3 === respuestasCorrectas.pregunta3) aciertos++;
+    if (respuesta4 === respuestasCorrectas.pregunta4) aciertos++;
+    if (respuesta5 === respuestasCorrectas.pregunta5) aciertos++;
+
+    if (aciertos <= 3) {
+        let emoTriste = "\u{1F61E}";
+        alert(`${emoTriste}Por favor, lea la documentación rockera.${emoTriste}`);
+    } else { //4 aciertos o más
+        let emoRock="\u{1F918}";
+        alert(`${emoRock}¡Enhorabuena, estás hecho un auténtico rockero!${emoRock}`);
+        event.target.submit();
+    }
+});
 
